a20e3c2d183a87b745f01fb08ea98fab7055614b
tajutzam <mohammadtajutzamzami07@gmail.com>
2026-01-10 06:46:32 +0700

    fix: aset

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 "resources/sass/app.scss",
                 "resources/js/app.js",
                 "resources/css/app.css",
+                "resources/css/bundle.css",
             ],
             refresh: true,
         }),
